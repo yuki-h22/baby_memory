0142ae611f8afe9fe35ecc940a8336e39e340dc5
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-20 21:09:20 +0900

    Update BabysTableSeeder.php

--- database/seeds/BabysTableSeeder.php
+++ database/seeds/BabysTableSeeder.php
@@ -11,6 +11,19 @@ class BabysTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+          // テーブルのクリア
+    DB::table('babys')->truncate();
+
+    // 初期データ用意（列名をキーとする連想配列）
+    $babys = [
+              ['name' => 'hinata',
+               'birthday' => 20181003]
+              ['name' => 'kotona',
+               'birthday' => 20190501]
+             ];
+        // 登録
+        foreach($babys as $baby) {
+        \App\Baby::create($baby);
+        }
     }
 }
