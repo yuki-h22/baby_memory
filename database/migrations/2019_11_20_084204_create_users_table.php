c2b610937350f4b00f6ea84b6fab5c6eecd1103d
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-20 19:14:24 +0900

    collection migrationfile

--- database/migrations/2019_11_20_084204_create_users_table.php
+++ database/migrations/2019_11_20_084204_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBabysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('babys', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->integer('birthday');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('babys');
     }
 }
