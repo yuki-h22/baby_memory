6b64b8d557446c4ad5ef99aea53d0429d0803244
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 19:24:24 +0900

    Update ContentController.php

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Content;
 class ContentController extends Controller
 {
-    //
+    public function index()
+    {
+        $contents = Content::all();
+        return view('content/index', compact('contents'));
+    }
+
+    public function create()
+    {
+        $content = new Content();
+        return view('baby/create', compact('baby'));
+    }
 }
