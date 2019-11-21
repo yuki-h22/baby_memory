b90b2de46eac85ba969920b43cef10ea6d08ab48
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 13:53:13 +0900

    Update BabyController.php

--- app/Http/Controllers/BabyController.php
+++ app/Http/Controllers/BabyController.php
@@ -8,18 +8,21 @@ use App\Baby;
 
 class BabyController extends Controller
 {
-    public function index()
-    {
-        // DBよりBookテーブルの値を全て取得
-        $babys = Baby::all();
-        // 取得した値をビュー「book/index」に渡す
-        return view('baby/index', compact('babys'));
-    }
-    public function edit($id)
-    {
-        // DBよりURIパラメータと同じIDを持つBookの情報を取得
-        $baby = Baby::findOrFail($id)
-        // 取得した値をビュー「book/edit」に渡す
-        return view('baby/edit', compact('baby'));
-    }
+  public function index()
+  {
+      // DBよりbabyテーブルの値を全て取得
+      $babys = Baby::all();
+
+      // 取得した値をビュー「baby/index」に渡す
+      return view('baby/index', compact('babys'));
+  }
+
+  public function edit($id)
+  {
+      // DBよりURIパラメータと同じIDを持つbabyの情報を取得
+      $baby = Baby::findOrFail($id);
+
+      // 取得した値をビュー「baby/edit」に渡す
+      return view('baby/edit', compact('baby'));
+  }
 }
