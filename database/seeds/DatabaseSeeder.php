587b175c99baf99ee81ef8ed2801812656d2ab6e
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 16:12:51 +0900

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+             // BabiesTableSeederを読み込むように指定
+     $this->call(BabiesTableSeeder::class);
     }
 }
