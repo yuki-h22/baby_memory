515287f284bc43e8f1efc20f193ce37a0083532c
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 16:13:01 +0900

    Update layout.blade.php

--- resources/views/baby/layout.blade.php
+++ resources/views/baby/layout.blade.php
@@ -1,5 +1,5 @@
 <head>
-  <title>Laravel Sample</title>
+  <title>Baby memorY</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 @yield('content')
