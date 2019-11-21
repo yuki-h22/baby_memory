e3ef9a13b04bdd69ca53dd5e60a5345388d02c0c
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 17:15:27 +0900

    Update message.blade.php

--- resources/views/baby/message.blade.php
+++ resources/views/baby/message.blade.php
@@ -1,6 +1,6 @@
 <div class = "row">
   <div class = "col-md-12">
-  @if ($erroes->any())
+  @if ($errors->any())
     <div class="alert alert-danger">
       <ul>
         @foreach($errors->all() as $error)
