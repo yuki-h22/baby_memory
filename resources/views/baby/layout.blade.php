b0771294d3e451c1128c2ea8efcb5e5bb374356e
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 20:29:22 +0900

    Update layout.blade.php

--- resources/views/baby/layout.blade.php
+++ resources/views/baby/layout.blade.php
@@ -2,4 +2,18 @@
   <title>Baby memorY</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
+
+<ul class="tab">
+  <li class="select">タブ1</li>
+  <li>タブ2</li>
+  <li>タブ3</li>
+  <li>タブ4</li>
+</ul>
+<ul class="content">
+  <li>親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。</li>
+  <li class="hide">なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。</li>
+  <li class="hide">新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。</li>
+  <li class="hide">弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。</li>
+</ul>
+
 @yield('content')
