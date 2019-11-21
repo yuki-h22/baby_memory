c8e22a6433877e7f6ce63ae77d5d4cef2ecf2a54
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 19:26:46 +0900

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return view('user/index', compact('users'));
+    }
+
+    public function create()
+    {
+        $user = new User();
+        return view('user/create', compact('user'));
+    }
 }
