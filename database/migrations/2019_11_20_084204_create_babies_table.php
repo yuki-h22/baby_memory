4948a63f14b16bb9b911b3fcb5fff1cde24225ca
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 17:20:01 +0900

    Update 2019_11_20_084204_create_babies_table.php

--- database/migrations/2019_11_20_084204_create_babies_table.php
+++ database/migrations/2019_11_20_084204_create_babies_table.php
@@ -17,6 +17,7 @@ class CreateBabiesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name', 50);
             $table->integer('birthday');
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
