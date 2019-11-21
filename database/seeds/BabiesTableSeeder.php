27a8922d91d2383b70191bdcf3b0b8fca23c4bf3
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 16:12:48 +0900

    Update BabiesTableSeeder.php

--- database/seeds/BabiesTableSeeder.php
+++ database/seeds/BabiesTableSeeder.php
@@ -4,26 +4,18 @@ use Illuminate\Database\Seeder;
 
 class BabiesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-             // BooksTableSeederを読み込むように指定
-     $this->call(BabiesTableSeeder::class);
-    
           // テーブルのクリア
     DB::table('babies')->truncate();
 
     // 初期データ用意（列名をキーとする連想配列）
     $babies = [
-              ['name' => 'hinata',
-               'birthday' => 20181003],
-              ['name' => 'kotona',
-               'birthday' => 20190501]
-             ];
+                ['name' => 'hinata',
+                'birthday' => 20181003],
+                ['name' => 'kotona',
+                'birthday' => 20190501]
+                ];
         // 登録
         foreach($babies as $baby) {
         \App\Baby::create($baby);
