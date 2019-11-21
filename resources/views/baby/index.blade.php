dd42e6af910c5f2332cc69a91098129cb03b1e43
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-21 15:10:22 +0900

    Update index.blade.php

--- resources/views/baby/index.blade.php
+++ resources/views/baby/index.blade.php
@@ -1,7 +1,5 @@
-<head>
-  <title>Laravel Sample</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+@extends('baby/layout')
+@section('content')
 <div class="container ops-main">
 <div class="row">
   <div class="col-md-12">
@@ -38,3 +36,4 @@
     <div><a href="/baby/create" class="btn btn-default">新規作成</a></div>
   </div>
 </div>
+@endsection
