58866c292135852802f6303c228398aee9fa8bf7
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-22 10:54:06 +0900

    Update layout.blade.php

--- resources/views/baby/layout.blade.php
+++ resources/views/baby/layout.blade.php
@@ -1,6 +1,7 @@
 <head>
   <title>Baby memorY</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/sass/tab.scss">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="js/script.js"></script>
   {{-- <script src="js/aa.js"></script> --}}
