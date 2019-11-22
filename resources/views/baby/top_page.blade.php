5efd1eeaeae28368cde4ac6c9ebaa2c7ef295698
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-22 13:45:55 +0900

    Update top_page.blade.php

---
+++ resources/views/baby/top_page.blade.php
@@ -0,0 +1,31 @@
+@section('top_page')
+<div class="side_bar">
+    <div class="side_bar_top">
+        <div class="side_bar_top_image">
+        </div>
+    </div>
+    <div class="side_bar_data">
+        <div class="side_bar_data_name"></div>
+        <div class="side_bar_data_birthday"></div>
+    </div>
+    <div class="side_bar_under">
+        <div class="side_bar_under_image">
+        </div>
+    </div>
+    <div class="side_bar_bottom">
+        <div class="side_bar_bottom_message"></div>
+    </div>
+<div class="main">
+    <div class="main_top"></div>
+    <div class="main_content">
+        <div class="main_content_data">
+            <div class="main_content_data_image"></div>
+            <div class="main_content_data_title"></div>
+        </div>
+        <div class="main_content_comment"></div>
+    </div>
+    <div class="main_edit_btn"></div>
+</div>
+
+
+@endsection
