a0f339952ec515c8bf40e958e5461829d33c7db1
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-22 23:07:37 +0900

    Update index.blade.php

--- resources/views/baby/index.blade.php
+++ resources/views/baby/index.blade.php
@@ -1,102 +1,37 @@
 @extends('baby/layout')
 
 @section('top')
-<div class="side_bar">
-    サイド
-      <div class="side_bar_top">
-        top
-          <div class="side_bar_top_image">
-              image
-          </div>
-      </div>
-      <div class="side_bar_data">
-          <div class="side_bar_data_name"></div>
-          <div class="side_bar_data_birthday"></div>
-      </div>
-      <div class="side_bar_under">
-          <div class="side_bar_under_image">
-          </div>
-      </div>
-      <div class="side_bar_bottom">
-          <div class="side_bar_bottom_message"></div>
-      </div>
-  <div class="main">
-      <div class="main_top"></div>
-      <div class="main_content">
-          <div class="main_content_data">
-              <div class="main_content_data_image"></div>
-              <div class="main_content_data_title"></div>
-          </div>
-          <div class="main_content_comment"></div>
-      </div>
-      <div class="main_edit_btn"></div>
-  </div>
+    @include('baby/tabs/top_page')
 @endsection
 
 @section('photo02')
-    gazou
+    @include('baby/tabs/album02')
 @endsection
 
 @section('photo35')
-    gazou
+    @include('baby/tabs/album35')
 @endsection
 
 @section('photo68')
-    gazou
+    @include('baby/tabs/album68')
 @endsection
 
 @section('photo911')
-    gazou
+    @include('baby/tabs/album911')
 @endsection
 
 @section('photo1')
-    gazou
+    @include('baby/tabs/album1')
 @endsection
 
-@section('album')
-    gazou
+@section('otheralbum')
+    @include('baby/tabs/otheralbum')
 @endsection
-
-
 
 @section('setting')
-<div class="container ops-main">
-<div class="row">
-  <div class="col-md-12">
-    <h3 class="ops-title">赤ちゃんたち</h3>
-  </div>
-</div>
-<div class="row">
-  <div class="col-md-11 col-md-offset-1">
-    <table class="table text-center">
-      <tr>
-        <th class="text-center">おなまえ</th>
-        <th class="text-center">お誕生日</th>
-        <th class="text-center">写真</th>
-        <th class="text-center">削除</th>
-      </tr>
-      @foreach($babies as $baby)
-      <tr>
-        <td>
-          <a href="/baby/{{ $baby->id }}/edit">{{ $baby->name }}</a>
-        </td>
-        <td>{{ $baby->birthday }}</td>
-        <td>{{ $baby->image }}</td>
-        <td>
-          <form action="/baby/{{ $baby->id }}" method="post">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <button type="submit" class="btn btn-xs btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-trash"></span></button>
-          </form>
-        </td>
-      </tr>
-      @endforeach
-    </table>
-    <div><a href="/baby/create" class="btn btn-default">赤ちゃんが生まれたよ</a></div>
-  </div>
-</div>
+    @include('baby/tabs/setting')
 @endsection
 
-@section('content')
-@include('baby/form', ['target' => 'update'])
-@endsection
+
+
+
