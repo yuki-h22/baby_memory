1ff5db43d3d980fd8ae2b208ac4070bb91694c57
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-22 23:08:08 +0900

    Create top_page.blade.php

---
+++ resources/views/baby/tabs/top_page.blade.php
@@ -0,0 +1,32 @@
+@section('top')
+<div class="side_bar">
+  サイド
+    <div class="side_bar_top">
+      top
+        <div class="side_bar_top_image">
+            image
+        </div>
+    </div>
+    <div class="side_bar_data">
+        <div class="side_bar_data_name"></div>
+        <div class="side_bar_data_birthday"></div>
+    </div>
+    <div class="side_bar_under">
+        <div class="side_bar_under_image">
+        </div>
+    </div>
+    <div class="side_bar_bottom">
+        <div class="side_bar_bottom_message"></div>
+    </div>
+<div class="main">
+    <div class="main_top"></div>
+    <div class="main_content">
+        <div class="main_content_data">
+            <div class="main_content_data_image"></div>
+            <div class="main_content_data_title"></div>
+        </div>
+        <div class="main_content_comment"></div>
+    </div>
+    <div class="main_edit_btn"></div>
+</div>
+@overwrite
