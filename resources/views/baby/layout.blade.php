53be523e5b493b0ee44279c988c21a331c5bd792
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-22 14:55:27 +0900

    Create layout.blade.php

---
+++ resources/views/baby/layout.blade.php
@@ -0,0 +1,34 @@
+<head>
+  <title>Baby memorY</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/sass/reset.scss">
+  <link rel="stylesheet" href="/css/sass/tab.scss">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+  {{-- <script src="js/aa.js"></script> --}}
+
+</head>
+
+<ul class="tab">
+  <li class="active">TOP Page</li>
+  <li>０〜2ヶ月</li>
+  <li>３〜5ヶ月</li>
+  <li>６〜8ヶ月</li>
+  <li>９〜11ヶ月</li>
+  <li>１歳〜</li>
+  <li>アルバム</li>
+  <li>設定</li>
+
+</ul>
+<ul class="content">
+  <li>@yield('top')</li>
+  <li class="hide">なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。</li>
+  <li class="hide">新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。</li>
+  <li class="hide">弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。</li>
+  <li class="hide">タブ2</li>
+  <li class="hide">タブ3</li>
+  <li class="hide">タブ4</li>
+  <li class="hide">@yield('content')</li>
+
+</ul>
+
