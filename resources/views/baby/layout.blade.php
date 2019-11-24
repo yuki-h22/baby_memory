68909184ae782f1b20f60bfe15cd39dbc848ea17
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-24 16:53:06 +0900

    Update layout.blade.php

--- resources/views/baby/layout.blade.php
+++ resources/views/baby/layout.blade.php
@@ -3,6 +3,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="/css/sass/reset.scss">
   <link rel="stylesheet" href="/css/sass/tab.scss">
+  <link rel="stylesheet" href="/css/sass/sidebar.scss">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="js/script.js"></script>
   {{-- <script src="js/aa.js"></script> --}}
