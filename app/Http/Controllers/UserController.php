470f424e2b67887b754f726378b71ac9db05ffbc
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-24 22:23:38 +0900

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,4 +17,18 @@ class UserController extends Controller
         $user = new User();
         return view('user/create', compact('user'));
     }
+
+    public function image(Request $request, User $user) {
+
+        // バリデーション省略
+        $originalImg = $request->user_image;
+      
+        if($originalImg->isValid()) {
+            $filePath = $originalImg->store('public');
+            $user->image = str_replace('public/', '', $filePath);
+            $user->save();
+            return redirect("/user/{$user->id}")->with('user', $user);
+      
+        }
+    }
 }
