35aea86ba701adde326d1d0780f88f44fcd878bd
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-25 16:14:15 +0900

    Create ContentsController.php

---
+++ app/Http/Controllers/ContentsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Content;
+class ContentController extends Controller
+{
+    public function index()
+    {
+        $contents = Content::all();
+        return view('content/index', compact('contents'));
+    }
+
+    public function create()
+    {
+        $content = new Content();
+        return view('content/create', compact('content'));
+    }
+}
