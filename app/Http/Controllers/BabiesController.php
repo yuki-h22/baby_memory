35c816f69b10f5559f57ea1c4f002abe48d9017a
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-25 16:57:26 +0900

    Update BabiesController.php

--- app/Http/Controllers/BabiesController.php
+++ app/Http/Controllers/BabiesController.php
@@ -22,6 +22,16 @@ class BabiesController extends Controller
       $baby = new Baby();
       $baby->name = $request->name;
       $baby->birthday = $request->birthday;
+      $params = $request->validate([
+        'image' => 'required|file|image|max:4000',]);
+      $file = $params['image'];
+      $image = \Image::make(file_get_contents($file->getRealPath()));
+      $image
+      ->save(public_path().'/images/'.$file->hashName())
+      ->resize(300, 300)
+      ->save(public_path().'/images/300-300-'.$file->hashName())
+      ->resize(500, 500)
+      ->save(public_path().'/images/500-500-'.$file->hashName());
       $baby->image = $request->image;
       $baby->save();
 
