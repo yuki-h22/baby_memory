627f3a5c28a3dd8763c1f39756b6f86078ae8331
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 15:41:42 +0900

    Update BabiesController.php

--- app/Http/Controllers/BabiesController.php
+++ app/Http/Controllers/BabiesController.php
@@ -7,52 +7,56 @@ use App\Baby;
 
 class BabiesController extends Controller
 {
-  public function index()
-  {
-      $babies = Baby::all();
-      return view('baby/index', compact('babies'));
-  }
+    public function index()
+    {
+        $babies = Baby::all();
+        return view('baby/index', compact('babies'));
+    }
 
-  public function store(BabyRequest $request)
-  {
-      $baby = new Baby();
-      $baby->name = $request->name;
-      $baby->birthday = $request->birthday;
-      $baby->image = $request->image;
-      $baby->save();
+    public function store(BabyRequest $request)
+    {
+        $baby = new Baby();
+        $baby->name = $request->name;
+        $baby->birthday = $request->birthday;
+        $baby->image = $request->image;
+        $baby->save();
 
-      return redirect("/baby");
-  }
+        return redirect("/baby");
+    }
 
-  public function create()
-  {
-      $baby = new Baby();
-      return view('baby/create', compact('baby'));
-  }
+    public function create()
+    {
+        $baby = new Baby();
+        return view('baby/create', compact('baby'));
+    }
 
-  public function edit($id)
-  {
-      $baby = Baby::findOrFail($id);
+    public function show($id)
+    {
+        return view('baby', ['baby' => Baby::findOrFail($id)]);
+    }
+    public function edit($id)
+    {
+        $baby = Baby::findOrFail($id);
 
-      return view('baby/edit', compact('baby'));
-  }
+        return view('baby/edit', compact('baby'));
+    }
 
-  public function update(BabyRequest $request, $id)
-  {
-      $baby = Baby::findOrFail($id);
-      $baby->name = $request->name;
-      $baby->birthday = $request->birthday;
-      $baby->save();
+    public function update(BabyRequest $request, $id)
+    {
+        $baby = Baby::findOrFail($id);
+        $baby->name = $request->name;
+        $baby->birthday = $request->birthday;
+        $baby->save();
 
-      return redirect("/baby");
-  }
+        return redirect("/baby");
+    }
 
-  public function destroy($id)
-  {
-      $baby = Baby::findOrFail($id);
-      $baby->delete();
+    public function destroy($id)
+    {
+        $baby = Baby::findOrFail($id);
+        $baby->delete();
 
-      return redirect("/baby");
-  }
+        return redirect("/baby");
+    }
 
 }
