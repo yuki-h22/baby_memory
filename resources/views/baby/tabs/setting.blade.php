861c177953ac3724e6634ede82c34145cc82bcc6
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 19:24:33 +0900

    Update setting.blade.php

--- resources/views/baby/tabs/setting.blade.php
+++ resources/views/baby/tabs/setting.blade.php
@@ -32,35 +32,9 @@
       </tr>
       @endforeach
     </table>
-    <div><a class="btn btn-default">赤ちゃんが生まれたよ</a></div>
+    <div><a href = "baby/create" class="btn btn-default">赤ちゃんが生まれたよ</a></div>
     {{-- href="/baby/create"  --}}
   </div>
 </div>
 
-<div class = "create_baby under_box">
-  <form method="post" action="baby/image_confirm" enctype="multipart/form-data">
-      <div class="form-group">
-          <label for="name">おなまえ</label>
-          <input type="text" class="form-control" name="name">
-      </div>
-      <div class="form-group">
-          <label for="price">お誕生日</label>
-          <input type="text" class="form-control" name="birthday">
-      </div>
-      <div class="form-group">
-          <label for="image">写真</label>
-              {{ csrf_field() }}
-          <fieldset>
-              <div>
-                  <input id="file" type="file" name="imagefile">
-                  @if ($errors->has('image'))
-                      {{ $errors->first('image') }}
-                  @endif
-              </div>
-          </fieldset>
-      </div>
-      <button type="submit" class="btn btn-default">登録</button>
-      <a href="/baby">戻る</a>
-  </form>
-</div>
 @endsection
